cdd214f3c1fa9adffcac6de32811855310ef41de
Mahak Jain Hingar <70008922+mahak-hingar@users.noreply.github.com>
2022-07-23 16:59:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./NavBar";
+import HeroSection from "./HeroSection";
+import AppSection from "./AppSection";
+import Cardsection from "./Cardsection";
+
+import Footer from "./Footer"
+
+
+
+
+
+
+
+const App = () => (
+    <div>
+        <Navbar />
+        <HeroSection />
+        <AppSection />
+        <Cardsection />
+        <Footer />
+        
+        
+        
+    </div>
+);
+export default App;
